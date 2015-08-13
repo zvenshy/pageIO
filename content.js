02161c3ba776117ae7e6bae4b1eeea99300ad1b0
zvenshy <zvenshy@gmail.com>
2015-08-13 14:22:47 +0800

    add

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+	//接受请求
 	chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
 		if (request.val) {
 			$('#addWikiAreaBox [name="wiki_type_title"]').val(request.val);
